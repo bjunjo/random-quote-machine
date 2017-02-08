54e59700a9c781b66195d5a8b38291cdd0884fd3
byoungjunjo <byoungjunjo@gmail.com>
2017-02-08 09:14:05 -0800

    second commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
 var quotes = [
   "It is vain to do with more what can be done with less. ―William of Occam",
-  "Progress is impossible without change, and those who cannot change their minds cannot change anything. -George Bernard Shaw",
-  "Being deeply loved by someone gives you strength, while loving someone deeply gives you courage. -Lao Tzu",
+  "Those who cannot change their minds cannot change anything. -George Bernard Shaw",
+  "Being deeply loved by someone gives you strength. -Lao Tzu",
   "Well done is better than well said. -Benjamin Franklin"
 ];
 
 var selectedQuote = quotes[Math.floor(Math.random() * quotes.length)];
 document.getElementById("selectedQuote").innerHTML = 
   '<p>' + selectedQuote + '</p>';
+// Twitter
+document.getElementById('twitter').onclick = function() {
+    var twtLink = 'http://twitter.com/home?status=' +selectedQuote;
+    window.open(twtLink,'_blank')
+};
   
 document.getElementById('button').onclick = function() {
   var selectedQuote = quotes[Math.floor(Math.random() * quotes.length)];
   document.getElementById("selectedQuote").innerHTML = 
   '<p>' + selectedQuote + '</p>';
+  // Twitter
+  document.getElementById('twitter').onclick = function() {
+    var twtLink = 'http://twitter.com/home?status=' +selectedQuote;
+    window.open(twtLink,'_blank')
+  };
 };
+
